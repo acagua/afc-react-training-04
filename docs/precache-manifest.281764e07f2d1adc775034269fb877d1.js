c6b9a15f0b65fe6f1c7026e2802d2cc7db5ccb67
Andres Cagua <af.cagua1011@gmail.com>
2020-09-21 21:25:45 -0500

    docs added

---
+++ docs/precache-manifest.281764e07f2d1adc775034269fb877d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2dbe1213f7d776b1644f6421ae5a448",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecf6ee3cf28740465eeb",
+    "url": "/static/css/main.d8219708.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf6ee3cf28740465eeb",
+    "url": "/static/js/main.a03b8709.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
